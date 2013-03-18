be87705404a756f3c9dbd59f1fbe12b2689538ba
Steve Flower <steve.flower@audient.com>
2013-03-18 22:42:01 +0000

    Made bootstrap file a Capistrano task.

--- blog-after/config/recipes/rbenv-bootstrap-ubuntu-12-04.rb
+++ blog-after/config/recipes/rbenv-bootstrap-ubuntu-12-04.rb
@@ -1,14 +1,18 @@
-# Install development tools:
-run "#{sudo} apt-get -y install build-essential"
+namespace :rbenv_bootstrap do
+  task :install, roles: :app do
+    # Install development tools:
+    run "#{sudo} apt-get -y install build-essential"
 
-# Packages required for compilation of some stdlib modules
-run "#{sudo} apt-get -y install tklib"
+    # Packages required for compilation of some stdlib modules
+    run "#{sudo} apt-get -y install tklib"
 
-# Extras for RubyGems and Rails:
-run "#{sudo} apt-get -y install zlib1g-dev libssl-dev"
+    # Extras for RubyGems and Rails:
+    run "#{sudo} apt-get -y install zlib1g-dev libssl-dev"
 
-# Readline Dev on Ubuntu 12.04 LTS:
-run "#{sudo} apt-get -y install libreadline-gplv2-dev"
+    # Readline Dev on Ubuntu 12.04 LTS:
+    run "#{sudo} apt-get -y install libreadline-gplv2-dev"
 
-# Install some nokogiri dependencies:
-run "#{sudo} apt-get -y install libxml2 libxml2-dev libxslt1-dev"
+    # Install some nokogiri dependencies:
+    run "#{sudo} apt-get -y install libxml2 libxml2-dev libxslt1-dev"
+  end
+end
